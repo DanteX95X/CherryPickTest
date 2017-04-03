a26ed4858af7be7e86be5f658442f6df4db3c78b
Dante <daniellewinski95@gmail.com>
2017-04-03 16:05:25 +0200

    Some ready to use code.

---
+++ Assets/Scripts/States/State.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.States
+{
+	public abstract class State : MonoBehaviour
+	{
+		#region variables
+
+		public static State currentState = null;
+
+		#endregion
+
+
+		#region methods
+
+		public void OnEnable()
+		{
+			currentState = this;
+			Init();
+		}
+
+		public void OnDisable()
+		{
+			CleanUp();
+		}
+
+		public void Update()
+		{
+			UpdateLoop();
+		}
+
+		public abstract void Init();
+
+		public abstract void UpdateLoop();
+
+		public abstract void CleanUp();
+
+		public void ChangeState<T>() where T : State
+		{
+			enabled = false;
+			GetComponent<T>().enabled = true;
+		}
+
+		#endregion
+	}
+}
